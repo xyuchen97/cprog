bb80bf34df8e2e10eb61fd8f238b2f9c0e3541a0
Coursera Learner <nobody@nowhere.nul>
2018-10-26 16:07:56 +0000

    D

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void squares ( int size1, int x_offset, int y_offset, int size2){ /* squares(int size1, int x_offset, int y_offset, int size2) { */
-  /* //compute the max of size1 and (x_offset + size2).  Call this w */
+void squares ( int size1, int x_offset, int y_offset, int size2){
   int w= size1>(x_offset +size2)?size1:(x_offset+ size2);
-  //compute the max of size1 and (y_offset + size2).  Call this h
   int h= size1>(y_offset+ size2)?size1:(y_offset+size2);
-  //count from 0 to h. Call the number you count with y
   for (int y=0; y < h; y++){
-    //count from 0 to w. Call the number you count with x
     for (int x=0; x< w; x++){
-      //check if  EITHER
-      if(((x_offset <= x && x< x_offset + size2)
-      //    ((x is between x_offset  and x_offset +size2) AND
-	  && (y==y_offset || y == y_offset + size2 -1))
-      //     y is equal to either y_offset OR y_offset + size2 - 1 )
-	 ||
-      //  OR
-	 ((y_offset <= y && y < y_offset + size2)
-      //    ((y is between y_offset and y_offset + size2) AND
+      if(((x_offset <= x && x< x_offset + size2) && (y==y_offset || y == y_offset + size2 -1)) || ((y_offset <= y && y < y_offset + size2)
 	  &&(x == x_offset || x== x_offset + size2 -1)))
-      //     x is equal to either x_offset OR x_offset + size2 -1)
-      // if so, print a *
 	{   printf("*");}
-      //if not,
-    else if 
-      // check if EITHER
-      ((x < size1 && (y==0 || y== size1 - 1))
-      //    x is less than size1 AND (y is either 0 or size1-1)
-       ||
-      // OR
-       (y< size1 && (x== 0 || x==size1 -1)))
-      //    y is less than size1 AND (x is either 0 or size1-1)
-      //if so, print a #
+    else if
+      ((x < size1 && (y==0 || y== size1 - 1)) || (y< size1 && (x== 0 || x==size1 -1)))
     {
 	  printf("#");}
       else{ printf(" ");}
-      
-      printf("\n");}}}
+    }
+      printf("\n");}}
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
